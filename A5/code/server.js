37d52facaa87ac1cd659d536040f46ccfcce7ebd
Emily <emily.ottesen@gmail.com>
2023-03-29 11:12:01 -0700

    http.js edits

--- A5/code/server.js
+++ A5/code/server.js
@@ -2,7 +2,7 @@ var url = require("url"),
 	querystring = require("querystring");
 var passport = require('passport');
 var fs = require('fs');
-	var dbURL = 'mongodb://44.200.209.114:27017/test';
+var dbURL = 'mongodb://44.200.209.114:27017/test';
 var path = require('path'),
   express = require('express'),
   db = require('mongoskin').db(dbURL);
@@ -50,12 +50,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.listen(8080);
 
 // DO NOT DO app.listen() unless we're testing this directly
-if (require.main === module) { app.listen(9999); }
+if (require.main === module) { app.listen(8080); }
 // Instead do export the app:
 else{ module.exports = app; }
 
 
-console.log("server running at http://localhost:9999")
+console.log("server running at http://localhost:8080")
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
